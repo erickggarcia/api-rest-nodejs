d9b087422e0d81dcebd63ab5bd72d59df39af2cb
Erick Garcia <erickgegarcia@gmail.com>
2023-08-31 22:21:50 -0300

    ajeitando arquivo database.ts com os dados corretos

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,9 @@
 import { knex as setupKnex, Knex } from 'knex'
-import { env } from './env/index'
+import { env } from './env'
 
 export const config: Knex.Config = {
-    client: env.DATABASE_URL,
-    connection: env.DATATABASE_CLIENT === 'sqlite' ?
+    client: env.DATABASE_CLIENT,
+    connection: env.DATABASE_CLIENT === 'sqlite' ?
         {
             filename: env.DATABASE_URL,
         } : env.DATABASE_URL,
